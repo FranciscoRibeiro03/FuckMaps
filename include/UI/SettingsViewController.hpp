e032c9db4007e7ff527bb7b22474e4f014255750
rui2015 <15705566+rui2015@users.noreply.github.com>
2021-09-07 21:57:39 +0100

    Feex some UI stuff

--- include/UI/SettingsViewController.hpp
+++ include/UI/SettingsViewController.hpp
@@ -3,8 +3,16 @@
 #include "custom-types/shared/macros.hpp"
 #include "HMUI/ViewController.hpp"
 
+#include "UnityEngine/UI/Toggle.hpp"
+#include "questui/shared/BeatSaberUI.hpp"
+
 DECLARE_CLASS_CODEGEN(FuckMaps, SettingsViewController, HMUI::ViewController,
 
     DECLARE_OVERRIDE_METHOD(void, DidActivate, il2cpp_utils::FindMethodUnsafe("HMUI", "ViewController", "DidActivate", 3), bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
+
+    DECLARE_INSTANCE_FIELD(UnityEngine::UI::Toggle*, crashHighBPMToggle);
+    DECLARE_INSTANCE_FIELD(QuestUI::IncrementSetting*, crashHighBPMValue);
+    DECLARE_INSTANCE_FIELD(UnityEngine::UI::Toggle*, crashLowBPMToggle);
+    DECLARE_INSTANCE_FIELD(QuestUI::IncrementSetting*, crashLowBPMValue);
 
 )
--- src/UI/SettingsViewController.cpp
+++ src/UI/SettingsViewController.cpp
@@ -29,37 +29,48 @@ void onCrashLowBPMValueChange(SettingsViewController* parent, float newValue) {
 
 void SettingsViewController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
 
-    if (!firstActivation) return;
+    if (firstActivation) {
 
-    getLogger().info("Creating UI options");
+        getLogger().info("Creating UI options");
 
-    auto layout = QuestUI::BeatSaberUI::CreateVerticalLayoutGroup(get_transform());
+        auto layout = QuestUI::BeatSaberUI::CreateVerticalLayoutGroup(get_transform());
 
-    // Delegates
-    auto onCrashHighBPMToggleDelegate = std::bind(onCrashHighBPMToggle, this, std::placeholders::_1);
-    auto onCrashHighBPMValueChangeDelegate = std::bind(onCrashHighBPMValueChange, this, std::placeholders::_1);
-    auto onCrashLowBPMToggleDelegate = std::bind(onCrashLowBPMToggle, this, std::placeholders::_1);
-    auto onCrashLowBPMValueChangeDelegate = std::bind(onCrashLowBPMValueChange, this, std::placeholders::_1);
-
-
-    // Anchored Position, for the CreateIncrementSetting function below
-    auto anchor = UnityEngine::Vector2(0, 0);
+        // Delegates
+        auto onCrashHighBPMToggleDelegate = std::bind(onCrashHighBPMToggle, this, std::placeholders::_1);
+        auto onCrashHighBPMValueChangeDelegate = std::bind(onCrashHighBPMValueChange, this, std::placeholders::_1);
+        auto onCrashLowBPMToggleDelegate = std::bind(onCrashLowBPMToggle, this, std::placeholders::_1);
+        auto onCrashLowBPMValueChangeDelegate = std::bind(onCrashLowBPMValueChange, this, std::placeholders::_1);
 
 
-    // Crash on High BPM Maps Toggle
-    auto crashHighBPMToggle = QuestUI::BeatSaberUI::CreateToggle(layout->get_transform(), "Crash on High BPM Maps?", getModConfig().crashHighBPMEnabled.GetValue(), onCrashHighBPMToggleDelegate);
-    QuestUI::BeatSaberUI::AddHoverHint(crashHighBPMToggle->get_gameObject(), "Enable/disable crashing the game when the song BPM is above a specified value (defined below).");
+        // Anchored Position, for the CreateIncrementSetting function below
+        auto anchor = UnityEngine::Vector2(0, 0);
 
-    // Crash on High BPM Maps Value
-    auto crashHighBPMValue = QuestUI::BeatSaberUI::CreateIncrementSetting(layout->get_transform(), "Max BPM", 0, 5.0f, getModConfig().crashHighBPM.GetValue(), true, false, 0.0f, 1000.0f, anchor,  onCrashHighBPMValueChangeDelegate);
-    QuestUI::BeatSaberUI::AddHoverHint(crashHighBPMValue->get_gameObject(), "Set the max BPM. If you try to play a song above this BPM and the toggle above is enabled, your game will crash.");
+
+        // Crash on High BPM Maps Toggle
+        SettingsViewController::crashHighBPMToggle = QuestUI::BeatSaberUI::CreateToggle(layout->get_transform(), "Crash on High BPM Maps?", getModConfig().crashHighBPMEnabled.GetValue(), onCrashHighBPMToggleDelegate);
+        QuestUI::BeatSaberUI::AddHoverHint(SettingsViewController::crashHighBPMToggle->get_gameObject(), "Enable/disable crashing the game when the song BPM is above a specified value (defined below).");
+
+        // Crash on High BPM Maps Value
+        SettingsViewController::crashHighBPMValue = QuestUI::BeatSaberUI::CreateIncrementSetting(layout->get_transform(), "Max BPM", 0, 5.0f, getModConfig().crashHighBPM.GetValue(), true, false, 0.0f, 1000.0f, anchor,  onCrashHighBPMValueChangeDelegate);
+        QuestUI::BeatSaberUI::AddHoverHint(SettingsViewController::crashHighBPMValue->get_gameObject(), "Set the max BPM. If you try to play a song above this BPM and the toggle above is enabled, your game will crash.");
     
-    // Crash on Low BPM Maps Toggle
-    auto crashLowBPMToggle = QuestUI::BeatSaberUI::CreateToggle(layout->get_transform(), "Crash on Low BPM Maps?", getModConfig().crashLowBPMEnabled.GetValue(), onCrashLowBPMToggleDelegate);
-    QuestUI::BeatSaberUI::AddHoverHint(crashLowBPMToggle->get_gameObject(), "Enable/disable crashing the game when the song BPM is below a specified value (defined below).");
+        // Crash on Low BPM Maps Toggle
+        SettingsViewController::crashLowBPMToggle = QuestUI::BeatSaberUI::CreateToggle(layout->get_transform(), "Crash on Low BPM Maps?", getModConfig().crashLowBPMEnabled.GetValue(), onCrashLowBPMToggleDelegate);
+        QuestUI::BeatSaberUI::AddHoverHint(SettingsViewController::crashLowBPMToggle->get_gameObject(), "Enable/disable crashing the game when the song BPM is below a specified value (defined below).");
 
-    // Crash on Low BPM Maps Value
-    auto crashLowBPMValue = QuestUI::BeatSaberUI::CreateIncrementSetting(layout->get_transform(), "Min BPM", 0, 5.0f, getModConfig().crashLowBPM.GetValue(), true, false, 0.0f, 1000.0f, anchor, onCrashLowBPMValueChangeDelegate);
-    QuestUI::BeatSaberUI::AddHoverHint(crashLowBPMValue->get_gameObject(), "Set the min BPM. If you try to play a song below this BPM and the toggle above is enabled, your game will crash.");
+        // Crash on Low BPM Maps Value
+        SettingsViewController::crashLowBPMValue = QuestUI::BeatSaberUI::CreateIncrementSetting(layout->get_transform(), "Min BPM", 0, 5.0f, getModConfig().crashLowBPM.GetValue(), true, false, 0.0f, 1000.0f, anchor, onCrashLowBPMValueChangeDelegate);
+        QuestUI::BeatSaberUI::AddHoverHint(SettingsViewController::crashLowBPMValue->get_gameObject(), "Set the min BPM. If you try to play a song below this BPM and the toggle above is enabled, your game will crash.");
+
+    } else {
+
+        SettingsViewController::crashHighBPMToggle->set_isOn(getModConfig().crashHighBPMEnabled.GetValue());
+        SettingsViewController::crashHighBPMValue->CurrentValue = getModConfig().crashHighBPM.GetValue();
+        SettingsViewController::crashHighBPMValue->UpdateValue();
+        SettingsViewController::crashLowBPMToggle->set_isOn(getModConfig().crashLowBPMEnabled.GetValue());
+        SettingsViewController::crashLowBPMValue->CurrentValue = getModConfig().crashLowBPM.GetValue();
+        SettingsViewController::crashLowBPMValue->UpdateValue();
+
+    }
 
 }
